2a42fef7a1a083ae284e5129710a954e8eeb239d
[Ali Yilmaz] <[aliyilmaztr96@gmail.com]>
2023-02-26 03:53:36 +0300

    EBOB-EKOK-Bulan-Program

--- src/ebob-ekokBulanProgram.java
+++ src/ebob-ekokBulanProgram.java
@@ -1,12 +1,6 @@
-# EBOB-EKOK-Bulan-Program
-Java EBOB-EKOK Bulan Program
-
-www.patika.dev
-
 import java.util.Scanner;
 
 public class Main {
-    
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
